2d9dd49fe9d280a0437f32c8a4b87d82290b7309
Jannik Möbius <moebius@kv.de>
2021-04-08 11:15:09 +0200

    main.py update

--- src/main.py
+++ src/main.py
@@ -26,7 +26,7 @@ class ImpressumCrawler:
         options.add_argument('--disable-dev-shm-usage')
         options.add_argument("--headless")
         options.add_argument("--disable-gpu")
-        #options.add_argument('--allow-insecure-localhost')
+        # options.add_argument('--allow-insecure-localhost')
 
         self.driver = webdriver.Chrome("/usr/bin/chromedriver", options=options)
 
@@ -48,8 +48,8 @@ class ImpressumCrawler:
         for id in data:
             self.searched_for_alt_impressums = False
             website = data[id]
-            #website = "http://www.schwendi.de/home/leben%2b_%2bfreizeit/schulen.html"
-            #website = "https://www.bast.de"
+            # website = "http://www.schwendi.de/home/leben%2b_%2bfreizeit/schulen.html"
+            # website = "https://www.bast.de"
 
             website = website.replace("https://", "").replace("http://", "").strip("/")
 
@@ -59,34 +59,34 @@ class ImpressumCrawler:
             try:
                 driver.get(url)
             except WebDriverException:
-                #print("WebDriverException #1 refreshing..")
+                # print("WebDriverException #1 refreshing..")
                 self.debug.append("WebDriverException #1 refreshing..")
                 try:
                     driver.refresh()
                 except WebDriverException as e:
-                    #print("No Website!")
+                    # print("No Website!")
                     self.debug.append("No Website!")
-                    #print(e)
+                    # print(e)
                     self.debug.append(e)
                     continue
             else:
                 # check if site is for sale
                 if "Website steht zum Verkauf" in driver.title:
-                    #print("Website is for Sale!")
+                    # print("Website is for Sale!")
                     self.debug.append("Website is for Sale!")
                     continue
 
                 # find Impressum link
                 if self.find_impressum() == -1:
                     # no Impressum found
-                    #print("No impressum found! find alternative Impressum..")
+                    # print("No impressum found! find alternative Impressum..")
                     self.debug.append("No impressum found! find alternative Impressum..")
 
                     if self.find_impressum_alternatives() == -1:
-                        #print("No alternative Impressums found. Look on current page for email")
+                        # print("No alternative Impressums found. Look on current page for email")
                         self.debug.append("No alternative Impressums found. Look on current page for email")
                         # if no impressum look on the current site for an email
-                        #continue
+                        # continue
 
                 source = driver.page_source
 
@@ -101,13 +101,13 @@ class ImpressumCrawler:
 
                 # if valid mails is empty and alternate impressums wasnt searched yet
                 if not valid_mails[int(id)] and not self.searched_for_alt_impressums:
-                    #print("Alt impressums lief noch nicht")
-                    #print(self.searched_for_alt_impressums)
+                    # print("Alt impressums lief noch nicht")
+                    # print(self.searched_for_alt_impressums)
                     self.debug.append("Alt impressums lief noch nicht")
                     self.debug.append(self.searched_for_alt_impressums)
 
                     if self.find_impressum_alternatives() == -1:
-                        #print("No alternative Impressums found.")
+                        # print("No alternative Impressums found.")
                         self.debug.append("No alternative Impressums found.")
                     else:
                         source = driver.page_source
@@ -133,11 +133,11 @@ class ImpressumCrawler:
                            r"+(?:\.|\(dot\))+[a-zA-Z0-9-.]+)")
             mails = p.findall(source)
 
-        #print(mails)
+        # print(mails)
         self.debug.append(mails)
 
         domain = website.replace("www.", "").replace("https://", "").replace("http://", "").replace("/", "")
-        #print("Domain: " + domain)
+        # print("Domain: " + domain)
         self.debug.append("Domain: " + domain)
 
         valid_mails = []
@@ -150,7 +150,9 @@ class ImpressumCrawler:
 
             # if email ends with .com instead of .de for example
             elif domain.rsplit(".", 1)[0] in email:
-                valid_mails.append(email.strip('.').replace("(at)", "@").replace("[at]", "@").replace(" @ ", "@").replace("(dot)", "."))
+                valid_mails.append(
+                    email.strip('.').replace("(at)", "@").replace("[at]", "@").replace(" @ ", "@").replace("(dot)",
+                                                                                                           "."))
 
             # if domain is to 80% email prefix for example: www.ib-shoefer.de and IB.shoefer@t-online.de
             elif self.compare_string(domain.rsplit(".", 1)[0], email.split("@")[0]) > 0.8:
@@ -158,24 +160,24 @@ class ImpressumCrawler:
                                    .replace(" @ ", "@").replace("(dot)", "."))
 
         if not valid_mails:
-            #print("Valid_mails empty!")
+            # print("Valid_mails empty!")
             self.debug.append("Valid_mails empty!")
 
             # check if domains contains two dots like shop.corilla.de
             if re.search(r'(?<!\.)\.\.(?!\.)', domain):
-                #print("domain contains two dots! splitting..")
+                # print("domain contains two dots! splitting..")
                 self.debug.append("domain contains two dots! splitting..")
                 domain = domain.split(".", 1)
 
             for email in mails:
                 if domain in email:
                     valid_mails.append(email.strip('.').replace("(at)", "@").replace("[at]", "@")
-                                   .replace(" @ ", "@").replace("(dot)", "."))
+                                       .replace(" @ ", "@").replace("(dot)", "."))
 
                 # if domain is to 80% email prefix for example: www.ib-shoefer.de and IB.shoefer@t-online.de
                 elif self.compare_string(domain.rsplit(".", 1)[0], email.split("@")[0]) > 0.7:
                     valid_mails.append(email.strip('.').replace("(at)", "@").replace("[at]", "@")
-                                   .replace(" @ ", "@").replace("(dot)", "."))
+                                       .replace(" @ ", "@").replace("(dot)", "."))
 
         if not valid_mails:
             valid_mails = mails
@@ -183,6 +185,7 @@ class ImpressumCrawler:
         # remove duplicates
         valid_mails = list(dict.fromkeys(valid_mails))
         print(valid_mails)
+        self.debug.append(valid_mails)
         return valid_mails
 
     def find_impressum(self):
@@ -192,15 +195,15 @@ class ImpressumCrawler:
             impressum_links = WebDriverWait(driver, 5).until(
                 lambda driver: driver.find_elements_by_link_text("Impressum"))
         except TimeoutException:
-            #print("No Impressum Link text")
+            # print("No Impressum Link text")
             self.debug.append("No Impressum Link text")
         else:
             for impressum_link in impressum_links:
-                #print("Found Impressum link by text:")
+                # print("Found Impressum link by text:")
                 self.debug.append("Found Impressum link by text:")
 
                 link = impressum_link.get_attribute("href")
-                #print(link)
+                # print(link)
                 self.debug.append(link)
 
                 # if javascript link click it
@@ -209,12 +212,12 @@ class ImpressumCrawler:
                         impressum_link.click()
                     except ElementClickInterceptedException:
                         if "javascript:void(0)" in link and impressum_link.get_attribute("onclick") is not None:
-                            #print("Executing script to click")
+                            # print("Executing script to click")
                             self.debug.append("Executing script to click")
                             driver.execute_script(impressum_link.get_attribute("onclick"))
 
                         else:
-                            #print("Element not clickable")
+                            # print("Element not clickable")
                             self.debug.append("Element not clickable")
                             # return -1
                 else:
@@ -226,12 +229,12 @@ class ImpressumCrawler:
             links = WebDriverWait(driver, 5).until(
                 lambda driver: driver.find_elements_by_tag_name("a"))
         except TimeoutException:
-            #print("No links on the page!")
+            # print("No links on the page!")
             self.debug.append("No links on the page!")
             return -1
 
         impressum_link = ""
-        #print links
+        # print links
         # for link in links:
         #     #print(link.get_attribute("href"))
         #     self.debug.append(link.get_attribute("href"))
@@ -264,7 +267,8 @@ class ImpressumCrawler:
         driver = self.driver
         self.searched_for_alt_impressums = True
 
-        impressum_alternatives = ["IMPRESSUM", "impressum", "Disclaimer", "disclaimer", "comany-info", "legal-notice", "Kontakt",
+        impressum_alternatives = ["IMPRESSUM", "impressum", "Disclaimer", "disclaimer", "comany-info", "legal-notice",
+                                  "Kontakt",
                                   "KONTAKT", "contact"]
 
         for alt in impressum_alternatives:
@@ -273,12 +277,12 @@ class ImpressumCrawler:
                 impressum_links = WebDriverWait(driver, 2).until(
                     lambda driver: driver.find_elements_by_link_text(alt))
             except TimeoutException:
-                #print("No " + alt +" Link text")
-                self.debug.append("No " + alt +" Link text")
+                # print("No " + alt +" Link text")
+                self.debug.append("No " + alt + " Link text")
             else:
                 for impressum_link in impressum_links:
-                    #print("Found Impressum link by text: " + alt)
-                    #print(impressum_link.get_attribute("href"))
+                    # print("Found Impressum link by text: " + alt)
+                    # print(impressum_link.get_attribute("href"))
                     self.debug.append("Found Impressum link by text: " + alt)
                     self.debug.append(impressum_link.get_attribute("href"))
 
@@ -290,12 +294,12 @@ class ImpressumCrawler:
                 links = WebDriverWait(driver, 2).until(
                     lambda driver: driver.find_elements_by_tag_name("a"))
             except TimeoutException:
-                #print("No " + alt + " links on the page!")
+                # print("No " + alt + " links on the page!")
                 self.debug.append("No " + alt + " links on the page!")
             else:
                 impressum_link = ""
                 # print links
-                #for link in links:
+                # for link in links:
                 #    print(link.get_attribute("href"))
 
                 for link in links:
@@ -327,29 +331,29 @@ class ImpressumCrawler:
             frames = WebDriverWait(driver, 2).until(
                 lambda driver: driver.find_elements_by_tag_name("frame"))
         except TimeoutException:
-            #print("No Frames found")
+            # print("No Frames found")
             self.debug.append("No Frames found")
 
         try:
             iframes = WebDriverWait(driver, 2).until(
                 lambda driver: driver.find_elements_by_tag_name("iframe"))
         except TimeoutException:
-            #print("No iFrames found")
+            # print("No iFrames found")
             self.debug.append("No iFrames found")
 
         frames.extend(iframes)
 
         if frames:
             for frame in frames:
-                #print("Frame:")
+                # print("Frame:")
                 self.debug.append("Frame:")
-                #try:
+                # try:
                 frame_name = frame.get_attribute("name")
-                #except
-                #print(frame_name)
+                # except
+                # print(frame_name)
                 self.debug.append(frame_name)
 
-                #driver.switch_to.frame(driver.find_element_by_xpath("//frame[@src='" + frame_src + "']"))
+                # driver.switch_to.frame(driver.find_element_by_xpath("//frame[@src='" + frame_src + "']"))
                 if frame_name:
                     try:
                         driver.switch_to.frame(frame_name)
@@ -395,12 +399,12 @@ class ImpressumCrawler:
         result = requests.post(url, data=data)
 
         if result.status_code != 200:
-            #print("Fehler! Status: " + str(result.status_code))
+            # print("Fehler! Status: " + str(result.status_code))
             self.debug.append("Fehler! Status: " + str(result.status_code))
             p = re.compile("<!--(.*?)-->", re.DOTALL)
             error = p.findall(result.text)[0]
 
-            #print(error)
+            # print(error)
             self.debug.append(error)
             return -1
 
@@ -422,26 +426,26 @@ class ImpressumCrawler:
             data = {"token": self.token, "host": host, "data": valid_mails}
 
         data = json.dumps(data)
-        #print("Submitting data:")
-        #print(data)
+        # print("Submitting data:")
+        # print(data)
         self.debug.append("submitting data:")
         self.debug.append(data)
 
         result = requests.post(url, json=data)
 
         if result.status_code != 200:
-            #print("Fehler! Status: " + str(result.status_code))
+            # print("Fehler! Status: " + str(result.status_code))
             self.debug.append("Fehler! Status: " + str(result.status_code))
             p = re.compile("<!--(.*?)-->", re.DOTALL)
             error = p.findall(result.text)[0]
-            #print(error)
+            # print(error)
             self.debug.append(error)
         else:
             self.debug.append("finished.")
 
     def click_link_and_search_again(self, website):
         driver = self.driver
-        #print("clicking any link and try again")
+        # print("clicking any link and try again")
         self.debug.append("clicking any link and try again")
         try:
             frames = WebDriverWait(driver, 3).until(
@@ -451,20 +455,20 @@ class ImpressumCrawler:
                 links = WebDriverWait(driver, 5).until(
                     lambda driver: driver.find_elements_by_tag_name("a"))
             except TimeoutException:
-                #print("No links on the page!")
+                # print("No links on the page!")
                 self.debug.append("No links on the page!")
             else:
-                #print(links[0].text)
+                # print(links[0].text)
                 self.debug.append(links[0].text)
 
                 if website in links[0].text:
                     links[0].click()
                     if self.find_impressum() == -1:
                         # no Impressum found
-                        #print("No impressum found! find alternative Impressum..")
+                        # print("No impressum found! find alternative Impressum..")
                         self.debug.append("No impressum found! find alternative Impressum..")
                         if self.find_impressum_alternatives() == -1:
-                            #print("No alternative Impressums found. Look on current page for email")
+                            # print("No alternative Impressums found. Look on current page for email")
                             self.debug.append("No alternative Impressums found. Look on current page for email")
                             return None
                             # if no impressum look on the current site for an email
@@ -490,14 +494,14 @@ class ImpressumCrawler:
             frames = WebDriverWait(self.driver, 5).until(
                 lambda driver: driver.find_elements_by_tag_name("frame"))
         except TimeoutException:
-            #print("No normal Frames found")
+            # print("No normal Frames found")
             self.debug.append("No normal Frames found")
 
         try:
             iframes = WebDriverWait(self.driver, 5).until(
                 lambda driver: driver.find_elements_by_tag_name("iframe"))
         except TimeoutException:
-            #print("No iFrames found")
+            # print("No iFrames found")
             self.debug.append("No iFrames found")
 
         frames.extend(iframes)
@@ -506,13 +510,13 @@ class ImpressumCrawler:
 
         if frames:
             for frame in frames:
-                #print("Frame:")
+                # print("Frame:")
                 self.debug.append("Frame:")
                 # try:
                 frame_name = frame.get_attribute("name")
                 # except
 
-                #print(frame_name)
+                # print(frame_name)
                 self.debug.append(frame_name)
 
                 # driver.switch_to.frame(driver.find_element_by_xpath("//frame[@src='" + frame_src + "']"))
@@ -525,7 +529,7 @@ class ImpressumCrawler:
 
                 driver.switch_to.default_content()
         else:
-            #print("No Frames or iFrames found")
+            # print("No Frames or iFrames found")
             self.debug.append("No Frames or iFrames found")
 
         # flatten list
@@ -546,10 +550,9 @@ class ImpressumCrawler:
 
 
 x = ImpressumCrawler()
-#x.run()
+# x.run()
 try:
     x.run()
 finally:
     x.tear_down()
     x.get_debug_info()
-
